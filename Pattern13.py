a11af502829226b7b1c4890ad5ea7e68f9c73a61
Akash <akashdkr170901@gmail.com>
2021-10-23 03:36:35 +0530

    done

---
+++ Pattern13.py
@@ -0,0 +1,7 @@
+n=int(input('Enter no of rows:'))
+for i in range(1,n+1):
+    for j in range(n,i,-1):
+        print(' ',end=' ')
+    for k in range(1,i+1):
+        print(i,end=' ')
+    print()
---
+++ Pattern14.py
@@ -0,0 +1,7 @@
+n=int(input('Enter no of rows:'))
+for i in range(n,0,-1):
+    for j in range(n,i,-1):
+        print(' ',end=' ')
+    for k in range(1,i+1):
+        print(i,end=' ')
+    print()
